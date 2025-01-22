207b22218065aef6d04ce2a3f772fc1a9f122310
wujianhua <1107015496@qq.com>
2025-01-22 18:26:33 +0800

    Remove obsolete DBML and SVG files; add new DBML schema and enhance App component for database loading
    - Deleted outdated `test.dbml` and `test.svg` files that were no longer in use.
    - Introduced a new `public/test.dbml` file defining a fresh database schema with tables for `users`, `posts`, and `follows`, including relationships between them.
    - Updated the `App.tsx` component to implement a database loading mechanism from local storage, utilizing the new DBML schema for improved data management.
    
    These changes streamline the project by removing unnecessary files and establishing a new database structure, enhancing the application's functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Suspense } from "react";
+import { Parser } from "@dbml/core";
+import { Suspense, useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ReactFlowProvider } from "reactflow";
 import AppLayout from "./components/app-layout";
 import Loader from "./components/custom/loader";
 import DbmlViewer from "./components/dbml-viewer";
 import TableViewer from "./components/table";
+import { useDatabaseStore } from "./data/store";
 
 // 创建路由配置
 const router = createBrowserRouter([
@@ -33,6 +35,22 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const { setDatabase } = useDatabaseStore();
+
+  useEffect(() => {
+    const database = localStorage.getItem("database");
+    if (!database) {
+      fetch("/test.dbml")
+        .then((res) => res.text())
+        .then((text) => {
+          localStorage.setItem("database", text);
+          const parser = new Parser();
+          const parsedDbml = parser.parse(text, "dbmlv2");
+          const database = parsedDbml.export();
+          setDatabase(database);
+        });
+    }
+  }, []);
   return (
     <ReactFlowProvider>
       <RouterProvider router={router} />
